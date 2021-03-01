61e023e85c3cf4c199a5b2512bfb4970c5daa5f9
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-01 04:58:32 +0000

    Added secondary games page, functioning games button. Issue to be fixed: home button on games page does not return to home.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log("hello");
+
+    const gameButton = document.getElementById('gamesButton');
+    const homeButton = document.getElementById('homeButton');
+    const favouriteButton = document.getElementById('favouritesButton');
+
+    gameButton.addEventListener('click', function() { assignHtmlPage("games") });
+    homeButton.addEventListener('click', function() { assignHtmlPage("home") });
+    favouriteButton.addEventListener('click', function() { assignHtmlPage("favourite") });
+
+    function assignHtmlPage(page) {
+        if(page === "home") {
+            window.location.href = '../index.html'
+        } else if(page === "games") {
+            window.location.href = '../games.html'
+        } else if(page === "favourite") {
+
+        }
+    }
