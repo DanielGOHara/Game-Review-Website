c30be17ce028f3d2370d7a4c2692b6c63df7794f
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-01 05:47:54 +0000

    Fixed all buttons which now function as intended. Added favourites HTML page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 
-    const gameButton = document.getElementById('gamesButton');
-    const homeButton = document.getElementById('homeButton');
-    const favouriteButton = document.getElementById('favouritesButton');
+    document.querySelectorAll('.gamesButton').forEach(item => {
+        item.addEventListener('click', event => {
+            assignHtmlPage("games");
+        });
+    });
 
-    gameButton.addEventListener('click', function() { assignHtmlPage("games") });
-    homeButton.addEventListener('click', function() { assignHtmlPage("home") });
-    favouriteButton.addEventListener('click', function() { assignHtmlPage("favourite") });
+    document.querySelectorAll('.homeButton').forEach(item => {
+        item.addEventListener('click', event => {
+            assignHtmlPage("home");
+        });
+    });
+
+    document.querySelectorAll('.favouritesButton').forEach(item => {
+        item.addEventListener('click', event => {
+            assignHtmlPage("favourite");
+        });
+    });
 
     function assignHtmlPage(page) {
-        if(page === "home") {
-            window.location.href = '../index.html'
-        } else if(page === "games") {
-            window.location.href = '../games.html'
-        } else if(page === "favourite") {
-
+        if (page === "home") {
+            window.location.href = 'index.html'
+        } else if (page === "games") {
+            window.location.href = 'games.html'
+        } else if (page === "favourite") {
+            window.location.href = 'favourites.html'
         }
     }
