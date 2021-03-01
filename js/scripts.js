ef9f71bf061c1571aca6a0f4489ef2cdfd9f43d4
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-01 07:34:40 +0000

    Left arrow not functioning, right arrow functioning but incorrectly game review slideshow works backwards.

--- js/scripts.js
+++ js/scripts.js
@@ -27,18 +27,37 @@
         }
     }
 
-    let gameIndex = 0;
+    let gameIndex = Math.floor((Math.random() * 3));
+    let timer = setTimeout(showGames, 20000);
     showGames();
 
-    function showGames() {
-        let i;
+    const prev = document.getElementsByClassName("prev");
+    const next = document.getElementsByClassName("next");
+
+    prev[0].addEventListener('click', event => {
+        nextGame(-1);
+    });
+
+    next[0].addEventListener('click', event => {
+        nextGame(1);
+    });
+
+    function nextGame(n) {
+        showGames(gameIndex += n);
+    }
+
+    function showGames(n) {
         const games = document.getElementsByClassName("gameArticle");
 
-        for(i = 0; i < games.length; i++) {
+        if(n > games.length) { gameIndex = 1 }
+        if(n < 1) { gameIndex = games.length }
+
+        for(let i = 0; i < games.length; i++) {
             games[i].style.display = "none";
         }
         gameIndex++;
+        clearTimeout(timer);
         if(gameIndex > games.length) { gameIndex = 1 }
         games[gameIndex - 1].style.display = "flex";
-        setTimeout(showGames, 20000);
+        timer = setTimeout(showGames, 20000);
     }
