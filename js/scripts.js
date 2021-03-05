a433f330e72c185be36742b855243e811e3aa298
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-05 09:42:14 +0000

    Expanded labels now working and expanding reviews correctly, all games now show a review correctly with each expand label expanding the correctly game. Copied desktop CSS over to tablet CSS section and have made some adjustments to suit the size, more to come. Changed the tablet declaration from just "min-width" to "min-width" and "max-width".

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,36 @@
             window.location.href = 'favourites.html'
         }
     }
+
+    /* Expand Button Listeners */
+
+    const butOne = document.getElementById('gameOneExpand');
+    const butTwo = document.getElementById('gameTwoExpand');
+    const butThree = document.getElementById('gameThreeExpand');
+    const butFour = document.getElementById('gameFourExpand');
+    const butFive = document.getElementById('gameFiveExpand');
+
+    butOne.addEventListener('click', event => {
+       document.getElementById('gameOne').classList.toggle("expanded");
+       document.getElementById('gameReviewOne').classList.toggle('hidden');
+    });
+
+    butTwo.addEventListener('click', event => {
+        document.getElementById('gameTwo').classList.toggle('expanded');
+        document.getElementById('gameReviewTwo').classList.toggle('hidden');
+    });
+
+    butThree.addEventListener('click', event => {
+        document.getElementById('gameThree').classList.toggle('expanded');
+        document.getElementById('gameReviewThree').classList.toggle('hidden');
+    });
+
+    butFour.addEventListener('click', event => {
+        document.getElementById('gameFour').classList.toggle('expanded');
+        document.getElementById('gameReviewFour').classList.toggle('hidden');
+    });
+
+    butFive.addEventListener('click', event => {
+        document.getElementById('gameFive').classList.toggle('expanded');
+        document.getElementById('gameReviewFive').classList.toggle('hidden');
+    });
