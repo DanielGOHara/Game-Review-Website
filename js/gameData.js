17d94b0cf8945e388958587675ac474ce9b5659d
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-05 12:39:23 +0000

    Copied CSS across from tablet styling to desktop for the leaderboard.

---
+++ js/gameData.js
@@ -0,0 +1,10 @@
+
+let data = '{ "games" : [ ' +
+    '{ "title":"TitanFall 2" , "release date":"28th October 2016" , description' +
+    ' Titanfall 2 is a first-person shooter video game,\n' +
+    ' developed by Respawn Entertainment and published\n' +
+    ' by Electronic Arts. A sequel to 2014\'s Titanfall,\n' +
+    ' the game was released worldwide on October 28,\n' +
+    ' 2016 for Windows, PlayStation 4, and Xbox One. ,' +
+    ' "score":"95" , "coverfilepath":"css/media/game_covers/titanfall_cover.png"' +
+    ' "covertext":"titanfall2cover" } ]}';
