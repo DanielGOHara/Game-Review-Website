44064b5f27522820613e7939c548a4c562c5660d
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-03-07 09:26:54 +0000

    Finished adding remaining games to "gameData" file including the reviews for each game.

--- js/gameData.js
+++ js/gameData.js
@@ -1,55 +1,124 @@
 
 export const data = [{
-        "title" : "Titanfall 2",
-        "description" : "Titanfall 2 is a first-person shooter video game, " +
-            "developed by Respawn Entertainment and published " +
-            "by Electronic Arts. A sequel to 2014's Titanfall, " +
-            "the game was released worldwide on October 28, " +
-            "2016 for Windows, PlayStation 4, and Xbox One.",
-        "release" : "28th October 2016",
-        "score" : "95",
-        "cover" : "css/media/game_covers/titanfall_cover.png",
-        "coveralt" : "titanfall2cover",
-        "review" : "A brilliant shooter which mix's up gameplay with the " +
-            "involvement of titans. The change of gameplay from " +
-            "these makes the gameplay interesting and fun with the " +
-            "tide of the game being able to change at anytime. In " +
-            "terms of graphics the game is massively ahead of its " +
-            "time with the game looking phenomenal, with optimisation " +
-            " being on point even with weaker computer hardware. Defiantly " +
-            "a game I see myself sinking hours into." }, {
-        "title" : "Spider-man",
-        "description" : "Marvel's Spider-Man is a 2018 action-adventure " +
-            "game developed by Insomniac Games and published " +
-            "by Sony Interactive Entertainment. Based on the " +
-            "Marvel Comics superhero Spider-Man, it is inspired " +
-            "by the long-running comic book mythology and adaptations " +
-            "in other media.",
-        "release" : "7th September 2016",
-        "score" : "92",
-        "cover" : "css/media/game_covers/spiderman_cover.png",
-        "coveralt" : "spidermancover",
-        "review" : "An amazing spiderman game that allows you to truly connect " +
-            "with spiderman with an interesting story that keeps you " +
-            "hooked for hours. The team at insomniac games really took " +
-            "time to perfect the movement mechanics with everything feeling " +
-            "very natural and fluid. The combat is outstanding and is just " +
-            "right with every encounter being enjoyable. As for the length " +
-            "of the game it felt perfect with plenty of plot points to keep " +
-            "you invested in the story and characters."}, {
-        "title" : "Fallout 4",
-        "description" : "Fallout 4 is an action role-playing game developed by Bethesda " +
-            "Game Studios and published by Bethesda Softworks. It is the " +
-            "fourth main game in the Fallout series and was released worldwide " +
-            "on November 10, 2015, for Microsoft Windows, PlayStation 4 and Xbox One.",
-        "release" : "10th November 2015",
-        "score" : "91",
-        "cover" : "css/media/game_covers/fallout4_cover.png",
-        "coveralt" : "fallout4cover",
-        "review" : "A fantastic fallout game which has a truly interesting and in depth story, " +
-            "whilst the game is beginning to show its age visually, it is still a brilliant" +
-            "title and a must play. The combination of traveling and building makes for a " +
-            "rewarding experience where you can witness your settlements slowly grow into" +
-            "sprawling towns. The added DLC further improves on the main game with out of this" +
-            "world stories in far away lands with new weapons, enemies and settlements."
-}];
+  "title" : "Titanfall 2",
+  "description" : "Titanfall 2 is a first-person shooter video game, " +
+    "developed by Respawn Entertainment and published " +
+    "by Electronic Arts. A sequel to 2014's Titanfall, " +
+    "the game was released worldwide on October 28, " +
+    "2016 for Windows, PlayStation 4, and Xbox One.",
+  "release" : "28th October 2016",
+  "score" : "95",
+  "cover" : "css/media/game_covers/titanfall_cover.png",
+  "coveralt" : "titanfall2cover",
+  "review" : "A brilliant shooter which mix's up gameplay with the " +
+    "involvement of titans. The change of gameplay from " +
+    "these makes the gameplay interesting and fun with the " +
+    "tide of the game being able to change at anytime. In " +
+    "terms of graphics the game is massively ahead of its " +
+    "time with the game looking phenomenal, with optimisation " +
+    "being on point even with weaker computer hardware. Defiantly " +
+    "a game I see myself sinking hours into." }, {
+
+  "title" : "Spider-man",
+  "description" : "Marvel's Spider-Man is a 2018 action-adventure " +
+    "game developed by Insomniac Games and published " +
+    "by Sony Interactive Entertainment. Based on the " +
+    "Marvel Comics superhero Spider-Man, it is inspired " +
+    "by the long-running comic book mythology and adaptations " +
+    "in other media.",
+  "release" : "7th September 2016",
+  "score" : "92",
+  "cover" : "css/media/game_covers/spiderman_cover.png",
+  "coveralt" : "spidermancover",
+  "review" : "An amazing spiderman game that allows you to truly connect " +
+    "with spiderman with an interesting story that keeps you " +
+    "hooked for hours. The team at insomniac games really took " +
+    "time to perfect the movement mechanics with everything feeling " +
+    "very natural and fluid. The combat is outstanding and is just " +
+    "right with every encounter being enjoyable. As for the length " +
+    "of the game it felt perfect with plenty of plot points to keep " +
+    "you invested in the story and characters."}, {
+
+  "title" : "Fallout 4",
+  "description" : "Fallout 4 is an action role-playing game developed by Bethesda " +
+    "Game Studios and published by Bethesda Softworks. It is the " +
+    "fourth main game in the Fallout series and was released worldwide " +
+    "on November 10, 2015, for Microsoft Windows, PlayStation 4 and Xbox One.",
+  "release" : "10th November 2015",
+  "score" : "91",
+  "cover" : "css/media/game_covers/fallout4_cover.png",
+  "coveralt" : "fallout4cover",
+  "review" : "A fantastic fallout game which has a truly interesting and in depth story, " +
+    "whilst the game is beginning to show its age visually, it is still a brilliant" +
+    "title and a must play. The combination of traveling and building makes for a " +
+    "rewarding experience where you can witness your settlements slowly grow into" +
+    "sprawling towns. The added DLC further improves on the main game with out of this" +
+    "world stories in far away lands with new weapons, enemies and settlements."}, {
+
+  "title" : "Detroit Become Human",
+  "description" : "Detroit: Become Human is a 2018 adventure game developed by Quantic " +
+    "Dream and published by Sony Interactive Entertainment. In a world of humans and " +
+    "androids you decide the fate of your story. Will you side with the humans or " +
+    "will you set androids free? The choice is yours.",
+  "release" : "24th April 2018",
+  "score" : "86",
+  "cover" : "css/media/game_covers/detroitbecomehuman_cover.png",
+  "coveralt" : "detroitbecomehumancover",
+  "review" : "A phenomenal new IP for Sony with an interesting gameplay tactic with choice " +
+    "being paramount and consequences drastically changing the story. The concept sounds " +
+    "fascinating and it is really brought to life well with the acting being outstanding. " +
+    "The story is so complex you could find yourself conflicted on what to do with some " +
+    "characters chasing others and your decision could be the end for some characters. Finishing " +
+    "the game I am very much looking forward to the next title in the series and look forward " +
+    "to exploring the world much more."}, {
+
+  "title" : "Call of Duty Cold War",
+  "description" : "Call of Duty: Black Ops Cold War is a 2020 first-person shooter video " +
+    "game developed by Treyarch and Raven Software and published by Activision. Building " +
+    "upon the previous entries in the series, takes us to the cold war bringing outstanding " +
+    "graphics and raytracing.",
+  "release" : "13th November 2020",
+  "score" : "82",
+  "cover" : "css/media/game_covers/coldwar_cover.png",
+  "coveralt" : "coldwarcover",
+  "review" : "Call of Duty Cold War is a beautiful addition to the franchise with the story " +
+    "taking place in the cold war era. The game gives you much more freedom that past titles " +
+    "with exploration encouraged and rewarded allowing you to take several approaches to a mission " +
+    "providing replayability to some extent. Building upon the past games the multiplayer gives " +
+    "the player a brilliant experience, with the guns being much more realistic and the gun play " +
+    "feeling more satisfying. Whilst many mechanics are new the franchise is beginning to feel " +
+    "bland and even with many of the new features if you are tired of the franchise this might be " +
+    "a pass."}, {
+
+  "title": "Spider-man Miles Morales",
+  "description" : "Marvel's Spider-Man: Miles Morales is a 2020 action-adventure " +
+    "game developed by Insomniac Games and published by Sony " +
+    "Interactive Entertainment for the PlayStation 4 and PlayStation 5.",
+  "release" : "12th November 2020",
+  "score" : "87",
+  "cover" : "css/media/game_covers/milesmorales_cover.png",
+  "coveralt" : "milesmoralescover",
+  "review" : "As a spin off title the game is short, around 10 hours but within these hours " +
+    "the developers have expanded upon the original giving miles his own fling. Recreating " +
+    "fighting, movement and ability mechanics bringing it all together and whilst it is similar " +
+    "it feels massively different to Spider-man. Miles's performance gives off the impression " +
+    "of a movie and from start to finish I was completely entrenched in the story and the worst " +
+    "part was the length of the game as I was thirsty for more once finished."}, {
+
+  "title" : "Resident Evil 2",
+  "description" : "Resident Evil 2 is a 2019 survival horror game " +
+    "developed and published by Capcom. A remake of the 1998 game of the " +
+    "same name, it was released for Microsoft Windows, PlayStation 4, and " +
+    "Xbox One on January 25, 2019.",
+  "release" : "25th January 2019",
+  "score" : "94",
+  "cover" : "css/media/game_covers/residentevil2_cover.png",
+  "coveralt" : "residentevil2cover",
+  "review" : "A suspenseful masterpiece with outstanding graphics, performance and plot. " +
+    "Whilst I am not an avid horror gamer I decided to try out Resident Evil 2 and it " +
+    "got me hooked, the atomsphere, gameplay mechanics and enemies made it very unique " +
+    "keeping me interested for hours, the structure of the gameplay allows players to explore " +
+    "moving the story forward at their own rate. After finishing the game I can truly say " +
+    "that it is a must play for horror game lovers and with the optimisation requires little " +
+    "in terms of hardware to run exceptionally well."
+  }];
