aac3b5e9a93609f38c1304a0d1e12775966c1264
Daniel O'Hara <p2435725@my365.dmu.ac.uk>
2021-04-29 12:37:07 +0100

    Added small paragraph to coming soon games for the review expressing my anticipation for the game over just stating it is coming soon.

--- js/gameData.js
+++ js/gameData.js
@@ -139,20 +139,20 @@ export const data = [{
 
   "title" : "Resident Evil 2",
   "description" : `Resident Evil 2 is a 2019 survival horror game
-    "developed and published by Capcom. A remake of the 1998 game of the
-    "same name, it was released for Microsoft Windows, PlayStation 4, and
-    "Xbox One on January 25, 2019.`,
+    developed and published by Capcom. A remake of the 1998 game of the
+    same name, it was released for Microsoft Windows, PlayStation 4, and
+    Xbox One on January 25, 2019.`,
   "release" : "25th January 2019",
   "score" : "90",
   "cover" : "css/media/game_covers/residentevil2_cover.png",
   "coveralt" : "residentevil2cover",
   "review" : `A suspenseful masterpiece with outstanding graphics, performance and plot.
-    "Whilst I am not an avid horror gamer I decided to try out Resident Evil 2 and it
-    "got me hooked, the atomsphere, gameplay mechanics and enemies made it very unique
-    "keeping me interested for hours, the structure of the gameplay allows players to explore
-    "moving the story forward at their own rate. After finishing the game I can truly say
-    "that it is a must play for horror game lovers and with the optimisation requires little
-    "in terms of hardware to run exceptionally well.`,
+    Whilst I am not an avid horror gamer I decided to try out Resident Evil 2 and it
+    got me hooked, the atomsphere, gameplay mechanics and enemies made it very unique
+    keeping me interested for hours, the structure of the gameplay allows players to explore
+    moving the story forward at their own rate. After finishing the game I can truly say
+    that it is a must play for horror game lovers and with the optimisation requires little
+    in terms of hardware to run exceptionally well.`,
   "favourite" : "No",
   "platform" : "PS4, Xbox One, PC",
   "trailer" : "https://www.youtube.com/embed/u3wS-Q2KBpk",
@@ -161,20 +161,20 @@ export const data = [{
 
   "title" : "Cyberpunk 2077",
   "description" : `Cyberpunk 2077 is a 2020 action role-playing video game developed and
-    "published by CD Projekt. The story takes place in Night City, an open world set in the
-    "Cyberpunk universe. The game has heavy RPG elements, with the player able to decide the
-    "fate of the city.`,
+    published by CD Projekt. The story takes place in Night City, an open world set in the
+    Cyberpunk universe. The game has heavy RPG elements, with the player able to decide the
+    fate of the city.`,
   "release" : "10th December 2020",
   "score" : "79",
   "cover" : "css/media/game_covers/cyberpunk2077_cover.png",
   "coveralt" : "cyberpunk2077cover",
   "review" : `A brilliant title with a vast world to explore that has a variety of culture
-    "and people to interact with, the sheer size of the city was astonishing at first allowing
-    "me to look past the issues the game faced on launch and truly get immersed into the story.
-    "The guns, vehicles and abilities all felt equally unique to one another making you want to
-    "explore each of them, the graphics were truly out of this world for a game of this size. Whilst
-    "it has many great features its not without its bugs which were many, something I hope will be resolved
-    "in future patches. However, if you can look past these it is well worth the experience to try out.`,
+    and people to interact with, the sheer size of the city was astonishing at first allowing
+    me to look past the issues the game faced on launch and truly get immersed into the story.
+    The guns, vehicles and abilities all felt equally unique to one another making you want to
+    explore each of them, the graphics were truly out of this world for a game of this size. Whilst
+    it has many great features its not without its bugs which were many, something I hope will be resolved
+    in future patches. However, if you can look past these it is well worth the experience to try out.`,
   "favourite" : "Yes",
   "platform" : "PS4, Xbox One, PC",
   "trailer" : "https://www.youtube.com/embed/8X2kIfS6fb8",
@@ -183,19 +183,19 @@ export const data = [{
 
   "title": "Death Stranding",
   "description" : `Death Stranding is an action game developed by Kojima Productions. It is the
-    "first game from director Hideo Kojima and Kojima Productions after their split from Konami
-    "in 2015. It was released for the PlayStation 4 in November 2019 and later for windows in 2020.`,
+    first game from director Hideo Kojima and Kojima Productions after their split from Konami
+    in 2015. It was released for the PlayStation 4 in November 2019 and later for windows in 2020.`,
   "release" : "8th November 2019",
   "score" : "98",
   "cover"  : "css/media/game_covers/deathstranding_cover.png",
   "coveralt" : "deathstrandingcover",
   "review" : `Probably one of my if not my most favourite title of 2020, after spending time on playstation
-    "it finally came to PC. Truly an immersive experience with outstanding graphics and performance with Norman
-    "providing an outstanding performance. Troy Baker also brought an evil mastermind villain who was throughout
-    "loved and hated, the idea sounding repetitive did not come across at all repetitive and somehow kept me
-    "interested throughout the story. The games design allowing other player buildings to appear in your work
-    "truly created an interest gameplay arch with the rain and snow speeding up time pushing to you plan your
-    "routes much better, especially when on foot. An absolute must play.`,
+    it finally came to PC. Truly an immersive experience with outstanding graphics and performance with Norman
+    providing an outstanding performance. Troy Baker also brought an evil mastermind villain who was throughout
+    loved and hated, the idea sounding repetitive did not come across at all repetitive and somehow kept me
+    interested throughout the story. The games design allowing other player buildings to appear in your work
+    truly created an interest gameplay arch with the rain and snow speeding up time pushing to you plan your
+    routes much better, especially when on foot. An absolute must play.`,
   "favourite" : "Yes",
   "platform" : "PS4, PS5, PC",
   "trailer" : "https://www.youtube.com/embed/tCI396HyhbQ",
@@ -204,13 +204,15 @@ export const data = [{
 
   "title" : "Far Cry 6",
   "description" : `Far Cry 6 is an upcoming first-person shooter game developed by Ubisoft Toronto and published
-    "by Ubisoft. It is the sixth main installment of the Far Cry series for Amazon Luna, Microsoft Windows,
-    "PlayStation 4, PlayStation 5, Xbox One and Xbox Series X. The game is set to be released in mid 2021.`,
+    by Ubisoft. It is the sixth main installment of the Far Cry series for Amazon Luna, Microsoft Windows,
+    PlayStation 4, PlayStation 5, Xbox One and Xbox Series X. The game is set to be released in mid 2021.`,
   "release" : "2021",
   "score" : "TBD",
   "cover" : "css/media/game_covers/farcry6_cover.png",
   "coveralt": "farcry6cover",
-  "review" : "Coming Soon!",
+  "review" : `Whilst not much has been released yet regarding details on the game, I am incredibly excited to 
+    see Giancarlo Esposito in action. The idea of the world, and sheer presence of Giancarlo on screen 
+    has kept me widely invested in the games release and I cannot wait to see what the story is about.`,
   "favourite" : "TBD",
   "platform" : "PS4, PS5, Xbox One, Xbox Series X, PC",
   "trailer" : "https://www.youtube.com/embed/mbGbLWevpC4",
@@ -219,13 +221,16 @@ export const data = [{
 
   "title" : "Resident Evil 8",
   "description" : `Resident Evil Village is an upcoming survival horror game developed and published by Capcom.
-    "It will be the tenth major installment in the Resident Evil series, and the sequel to Resident Evil 7: Biohazard.
-    "It also includes an online multiplayer mode, Resident Evil RE:Verse.`,
+    It will be the tenth major installment in the Resident Evil series, and the sequel to Resident Evil 7: Biohazard.
+    It also includes an online multiplayer mode, Resident Evil RE:Verse.`,
   "release" : "7th May 2021",
   "score" : "TBD",
   "cover" : "css/media/game_covers/residentevil8_cover.png",
   "coveralt" : "residentevil8cover",
-  "review" : "Coming Soon!",
+  "review" : `Being the next game in the resident evil franchise I am immensely excited to see what this one has 
+    to offer, the trailer and demos are providing an awesome experience and I am defiantly eager to see 
+    what the game has to offer. Whilst the previous game resident evil 3 fell a little short this title
+    definitely looks well worth the wait and I cannot wait to see what Ethan has in store.`,
   "favourite" : "TBD",
   "platform" : "PS4, PS5, Xbox One, Xbox Series X, PC",
   "trailer" : "https://www.youtube.com/embed/BDlUtPTQEOk",
@@ -234,13 +239,15 @@ export const data = [{
 
   "title" : "Horizon Forbidden West",
   "description" : `Horizon Forbidden West is an upcoming action role-playing game developed by Guerrilla Games and
-    "published by Sony Interactive Entertainment. It is set to be released in 2021 for the PlayStation 4 and
-    "PlayStation 5. It is the sequel to Horizon Zero Dawn.`,
+    published by Sony Interactive Entertainment. It is set to be released in 2021 for the PlayStation 4 and
+    PlayStation 5. It is the sequel to Horizon Zero Dawn.`,
   "release" : "2021",
   "score" : "TBD",
   "cover" : "css/media/game_covers/horizonforbiddenwest_cover.png",
   "coveralt" : "horizonforbiddenwestcover",
-  "review" : "Coming Soon!",
+  "review" : `Whilst not much has been released detail wise for the game, I cannot help but feel overly excited 
+    to get my hands on it. The previous title was out of this world and made me fall in love with the
+    story and lore behind the entire world. I look forward to seeing what the game has in store for Aloy`,
   "favourite" : "TBD",
   "platform" : "PS4, PS5",
   "trailer" : "https://www.youtube.com/embed/Lq594XmpPBg",
@@ -249,13 +256,16 @@ export const data = [{
 
   "title" : "Ratchet and Clank Rift Apart",
   "description" : `Ratchet & Clank: Rift Apart is an upcoming third-person shooter platform game developed by
-    "Insomniac Games and published by Sony Interactive Entertainment for the PlayStation 5. It will serve as a
-    "canonical continuation to Ratchet & Clank: Into the Nexus.`,
+    Insomniac Games and published by Sony Interactive Entertainment for the PlayStation 5. It will serve as a
+    canonical continuation to Ratchet & Clank: Into the Nexus.`,
   "release" : "11th June 2021",
   "score" : "TBD",
   "cover" : "css/media/game_covers/ratchet&clankriftapart_cover.png",
   "coveralt" : "ratchet&clankriftapartcover",
-  "review" : "Coming Soon!",
+  "review" : `Having been a fan of the series since I was young, I am eager to see what direction the studio took 
+    the series in, I expect with the new technology introduced by Sony it will be an awesome experience.
+    With the games idea being one of a kind, I eagerly await to see how this is implemented whether it be
+    simply linear story based, or open world exploration.`,
   "favourite" : "TBD",
   "platform" : "PS5",
   "trailer" : "https://www.youtube.com/embed/ai3o0XtrnM8",
@@ -264,13 +274,16 @@ export const data = [{
 
   "title" : "Biomutant",
   "description" : `Biomutant is an upcoming action role-playing game developed by Swedish developer Experiment
-    "101 and published by THQ Nordic. The game will be released on May 25, 2021 for Microsoft Windows,
-    "PlayStation 4, and Xbox One.`,
+    101 and published by THQ Nordic. The game will be released on May 25, 2021 for Microsoft Windows,
+    PlayStation 4, and Xbox One.`,
   "release" : "25th May 2021",
   "score" : "TBD",
   "cover" : "css/media/game_covers/biomutant_cover.png",
   "coveralt" : "biomutantcover",
-  "review" : "Coming Soon!",
+  "review" : `Whilst one of my least anticipated games I love RPG style open world games where I can explore
+    unhindered, being able to pick and choose my story is something i treasure. The expansive nature of the 
+    biomutant world, the vehicles, character customisation and boss fights should make for an immersive quarky
+    title that can be enjoyed alongside other bigger well known games.`,
   "favourite" : "TBD",
   "platform" : "PS4, Xbox One, PC",
   "trailer" : "https://www.youtube.com/embed/yXIE68V3ajE",
